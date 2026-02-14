e2c10e12ca057d409e8053ce9b6c68af19294a65
Fu-Yilei <fuyilei96@outlook.com>
2026-02-13 18:41:15 -0600

    Add GitHub PyPI publish workflow and rename package to methdb

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mdb',
+    name='methdb',
     version='0.0.2',
     packages=find_packages(),
     url='https://github.com/Fu-Yilei/mdb',
